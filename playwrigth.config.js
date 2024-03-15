4372eb06834018e0faf4b0734ef163adad4ef43c
Ingrith Santamaria <ingrithsanta@gmail.com>
2024-03-15 14:52:53 -0500

    Configurations Initial settings

---
+++ playwrigth.config.js
@@ -0,0 +1,28 @@
+import { devices } from '@playwright/test';
+export default {
+  testDir: './tests',
+  projects: [
+    {
+      name: 'Chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    // {
+    //   name: 'Firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
+    // {
+    //   name: 'WebKit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
+  ],
+  retries: 1,
+  reporter: 'html',
+  use: {
+    baseURL: 'https://www.adidas.com',
+    screenshot: 'off',
+    video: 'off',
+    timezoneId: 'America/New_York',
+  },
+  workers: 4,
+  timeout: 30000,
+};
